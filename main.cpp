36226caddfdd95a962f0bf7505986892e5f272bb
Thushara B S <abhibajimar07@gmail.com>
2026-03-02 20:57:37 +0530

    Add menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int menu(){
+    int choose;
+
     cout<<" NAMASTE!!\n\nWELCOME TO VAULTERY!!"<<endl;
+    cout<<"1. Create File\n";
+    cout<<"2. View All Files\n";
+    cout<<"3. Search File\n";
+    cout<<"4. Modify File\n";
+    cout<<"5. Delete File\n";
+    cout<<"6. Exit\n";
+
+    cout<<"What do you want to do?\n";
+    cin>>choose;
+
+    return choose;
+}
+
+int main(){
+
 
     return 0;
 }
